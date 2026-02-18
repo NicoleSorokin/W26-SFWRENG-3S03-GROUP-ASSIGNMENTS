bfcde5914999df7e94faca8dd0a98d9b482ab151
Ashaz Ahmad <ahmad.ashaz41@gmail.com>
2026-02-18 13:25:37 -0500

    Completed Q4b

---
+++ assignment1/Q4/src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return 2.0;
+    }
+}
